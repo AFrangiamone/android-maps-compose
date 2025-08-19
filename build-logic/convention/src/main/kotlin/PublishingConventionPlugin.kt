40940ff57e62150b7f9abd8d4d461c9bc0a36516
Alessandro Frangiamone <afrangiamone@thefork.com>
2025-08-19 16:58:29 +0200

    Update PublishingConventionPlugin.kt

--- build-logic/convention/src/main/kotlin/PublishingConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/PublishingConventionPlugin.kt
@@ -14,7 +14,7 @@ class PublishingConventionPlugin : Plugin<Project> {
 
             applyPlugins()
             configureJacoco()
-            configureVanniktechPublishing()
+            // configureVanniktechPublishing()
         }
     }
 
